c82749aa1a2deeedd858dafb29065500eb8ce664
Thorstein Løkensgard <loekensgard@gmail.com>
2023-12-03 18:31:52 +0100

    refactor: run all puzzles from one method

--- 2023/Solutions.cs
+++ 2023/Solutions.cs
@@ -3,18 +3,14 @@ using adventofcode._2023.Day2;
 using AdventOfCode._2023.Day3;
 
 // Read input from file
-var day1 = System.IO.File.ReadAllLines("../../../2023/Day1/input.txt");
-var day2 = System.IO.File.ReadAllLines("../../../2023/Day2/input.txt");
-var day3 = System.IO.File.ReadAllLines("../../../2023/Day3/input.txt");
+SolvePuzzles("../../../2023/Day1/input.txt", Day1.SolvePartOne, Day1.SolvePartTwo);
+SolvePuzzles("../../../2023/Day2/input.txt", Day2.SolvePartOne, Day2.SolvePartTwo);
+SolvePuzzles("../../../2023/Day3/input.txt", Day3.SolvePartOne, Day3.SolvePartTwo);
 
-// Day 1
-Console.WriteLine("Puzzle One Day1: " + Day1.SolvePartOne(day1));
-Console.WriteLine("Puzzle Two Day1: " + Day1.SolvePartTwo(day1));
+static void SolvePuzzles(string filePath, Func<string[], int> solvePartOne, Func<string[], int> solvePartTwo)
+{
+    var input = File.ReadAllLines(filePath);
 
-// Day 2
-Console.WriteLine("Puzzle One Day2: " + Day2.SolvePartOne(day2));
-Console.WriteLine("Puzzle Two Day2: " + Day2.SolvePartTwo(day2));
-
-// Day 3
-Console.WriteLine("Puzzle One Day3: " + Day3.SolvePartOne(day3));
-Console.WriteLine("Puzzle Two Day3: " + Day3.SolvePartTwo(day3));
+    Console.WriteLine($"Puzzle One: {solvePartOne(input)}");
+    Console.WriteLine($"Puzzle Two: {solvePartTwo(input)}");
+}

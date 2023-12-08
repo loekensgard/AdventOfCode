5f9d61100b7972728e717782e70e4ce74bee6187
Thorstein Løkensgard <thorstein.lokensgard@intility.no>
2023-12-08 14:50:09 +0100

    feat: solve day 8 part 1

--- 2023/Day8/Day8.cs
+++ 2023/Day8/Day8.cs
@@ -4,12 +4,83 @@
     {
         public static string SolvePartOne(string[] lines)
         {
-            return 0.ToString();
+            var instructions = lines[0];
+            var network = GetNodes(lines);
+
+            var currentNode = "AAA";
+            var steps = 0;
+
+            while (true)
+            {
+                bool foundZZZ = false;
+
+                for (int i = 0; i < instructions.Length; i++)
+                {
+                    char direction = instructions[i];
+                    currentNode = (direction == 'L') ? network[currentNode].left : network[currentNode].right;
+                    steps++;
+
+                    if (currentNode == "ZZZ")
+                    {
+                        foundZZZ = true;
+                        break;
+                    }
+                }
+
+                if (foundZZZ)
+                {
+                    break;
+                }
+            }
+
+            return steps.ToString();
+        }
+
+        private static Dictionary<string, (string left, string right)> GetNodes(string[] lines)
+        {
+            Dictionary<string, (string left, string right)> network = new();
+
+
+            foreach (string line in lines.Skip(1))
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    string[] parts = line.Split('=');
+
+                    if (parts.Length == 2)
+                    {
+                        // Trim any extra spaces
+                        string key = parts[0].Trim();
+                        string values = parts[1].Trim(' ', '(', ')');
+
+                        // Split the values into left and right
+                        string[] valueParts = values.Split(',');
+
+                        if (valueParts.Length == 2)
+                        {
+                            // Trim any extra spaces from each value
+                            string left = valueParts[0].Trim();
+                            string right = valueParts[1].Trim();
+
+                            // Add to the dictionary
+                            network.Add(key, (left, right));
+                        }
+                    }
+                }
+            }
+
+            return network;
         }
 
         public static string SolvePartTwo(string[] lines)
         {
+            var instructions = lines[0];
+            var network = GetNodes(lines);
+
+            //Bleh
+
             return 0.ToString();
         }
+
     }
 }
--- 2023/Solutions.cs
+++ 2023/Solutions.cs
@@ -4,6 +4,7 @@ using AdventOfCode._2023.Day3;
 using AdventOfCode._2023.Day4;
 using AdventOfCode._2023.Day6;
 using AdventOfCode._2023.Day7;
+using AdventOfCode._2023.Day8;
 
 // Read input from file
 SolvePuzzles("../../../2023/Day1/input.txt", Day1.SolvePartOne, Day1.SolvePartTwo);
@@ -12,6 +13,7 @@ SolvePuzzles("../../../2023/Day3/input.txt", Day3.SolvePartOne, Day3.SolvePartTw
 SolvePuzzles("../../../2023/Day4/input.txt", Day4.SolvePartOne, Day4.SolvePartTwo);
 SolvePuzzles("../../../2023/Day6/input.txt", Day6.SolvePartOne, Day6.SolvePartTwo);
 SolvePuzzles("../../../2023/Day7/input.txt", Day7.SolvePartOne, Day7.SolvePartTwo);
+SolvePuzzles("../../../2023/Day8/input.txt", Day8.SolvePartOne, Day8.SolvePartTwo);
 
 
 static void SolvePuzzles(string filePath, Func<string[], string> solvePartOne, Func<string[], string> solvePartTwo)
